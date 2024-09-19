dd0fd823bfc908001e3e2b467fed25b3c442d7f4
Tianyi Ma <tianyima@TianyideMacBook-Pro.local>
2024-09-19 00:03:37 -0700

    lab1 update

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,8 +3,16 @@ import Lab1 from "./Lab1";
 export default function Labs() {
   return (
     <div id="wd-labs">
-      <h1>Labs</h1>
+      <h1>Tianyi Ma - Labs</h1>
       <Lab1 />
+      <a
+        href="https://github.com/GracyMa/kanbas-react-web-app"
+        id="wd-github"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub Repository
+      </a>
     </div>
-);}
-
+  );
+}
